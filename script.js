2f506839349afbdbcc0e36636bea79c6eb30ab36
Joseph Matthews <poohna28@yahoo.com>
2026-04-30 02:01:27 -0700

    luxury design update

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-document.querySelector("button").addEventListener("click", () => {
-  document.body.style.opacity = "0";
-  setTimeout(() => {
-    alert("Welcome to Guilty of Desire 😈");
-    document.body.style.opacity = "1";
-  }, 500);
+document.getElementById("enterBtn").addEventListener("click", function() {
+  document.querySelector(".next-section").scrollIntoView({
+    behavior: "smooth"
+  });
 });
